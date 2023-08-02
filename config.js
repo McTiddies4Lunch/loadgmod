4dbc9655cc239cecd40f39c553957e7879b3063f
McTiddies4Lunch <122637851+McTiddies4Lunch@users.noreply.github.com>
2023-08-01 23:30:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ config.logo_use_image = false
 config.logo_image = "logo.png"
 
 // Text to be shown on logo
-config.logo_text = "TDRP | Serious RP"
+config.logo_text = "TDRP Serious RP"
 
 // Use the server's name for the logo
 config.logo_servername = False
